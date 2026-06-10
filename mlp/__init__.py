12a62d657d52514165a4d10e64050021a2c788ff
funnyweird <paulo.souza@sou.inteli.edu.br>
2026-06-10 17:16:28 -0300

    chore: atualiza __init__.py e requirements.txt

---
+++ mlp/__init__.py
@@ -0,0 +1,4 @@
+from .network import MLP
+from .optimizers import SGD, Adam
+from .activations import relu, relu_derivative, softmax
+from .losses import cross_entropy_loss, cross_entropy_derivative
